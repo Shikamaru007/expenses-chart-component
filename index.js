c4f7ba59ca3a99dd561d48d7da1b4c58486a1e08
froZin <bolutolu12@gmail.com>
2024-11-03 19:24:32 +0100

    Finally done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Fetch the json file and its data.
 fetch(`data.json`).then(response => {
     if(!response.ok){
         throw new Error(window.alert("couldn't fetch file"));
@@ -6,21 +7,23 @@ fetch(`data.json`).then(response => {
     }
 }).then(data => displayBars(data)).catch(error => {console.log(error)});
 
+// Function to display the bars according to the amount per day.
 function displayBars(data){
 
     const chart = document.getElementById("chart");
-    const maximumAmount = Math.max(...data.map(dayDAta => dayDAta.amount));
+    const maximumAmount = Math.max(...data.map(dayDAta => dayDAta.amount)); // Get the maximum amount to use to propotion the other bars.
 
-
-
+    // Get today's day to identify the bar to be highlighted.
     const date = new Date();
     let today = date.getDay();
 
+    // Sets and displays each of the bars from the data.json file to the webpage.
     data.forEach(item => {
-        const barContainer = document.createElement("div");
-        const bar = document.createElement("div");
-        const value = document.createElement("div");
-        const modal = document.createElement("div");
+        const barContainer = document.createElement("div"); // Bar and day container.
+        const bar = document.createElement("div"); // Bar to be displayed.
+        const value = document.createElement("div"); // Day to be displayed.
+        const modal = document.createElement("div"); // Amount modal to be displayed on hover.
+
         bar.classList.add("bar");
         barContainer.classList.add("bar-container");
         value.classList.add("value");
@@ -29,11 +32,12 @@ function displayBars(data){
         value.innerText = item.day;
         modal.innerText = `$${item.amount.toFixed(2)}`
 
+        // Checks for the bar representing today and highlights it.
         if(today === item.id){
             bar.classList.add("today");
         }
 
-        const barHeight = (item.amount / maximumAmount) * 80;
+        const barHeight = (item.amount / maximumAmount) * 80; // Calculates height for the bars in the container.
 
         bar.style.height = `${barHeight}%`;
 
